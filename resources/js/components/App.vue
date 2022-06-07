1eb6f88852d4cb65edf6c8f7823b3a16951d6955
Santi Dev <santi.chpy@gmail.com>
2022-06-07 20:57:48 +0700

    setup

---
+++ resources/js/components/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="alert alert-primary" role="alert">
+    {{message}}
+  </div>
+</template>
+
+<script>
+export default {
+      name : "App",
+  data() {
+    return {
+      message: 'จาก Conponents VueJs นะครับ'
+    }
+  }
+}
+</script>
